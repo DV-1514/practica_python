10fe7ede7a0c0d6b4ca810825ecec68f224ec3bb
Dante <dantevalles2016@yahoo.com>
2021-04-17 18:16:16 -0300

    Practica

--- diccionarios.py
+++ diccionarios.py
@@ -1,5 +1,25 @@
-divisa = input("Introduce una divisa: ")
+'''1. Escribir un programa que guarde en una variable el diccionario {'Euro':
+'€', 'Dollar':'$', 'Yen':'¥'}, pregunte al usuario por una divisa y muestre 
+su símbolo o un mensaje de aviso si la divisa no está en el diccionario.'''
 
-monedas = {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+# divisa = input("Introduce una divisa: ")
+# monedas = {'Euro':'€', 'Dollar':'$', 'Yen':'¥'}
+# print(monedas.get(divisa.title(), "La divisa no está."))
 
-print(monedas.get(divisa.title(), "La divisa no está."))
+'''2. Escribir un programa que pregunte al usuario su nombre, edad, dirección 
+y teléfono y lo guarde en un diccionario. Después debe mostrar por pantalla 
+el mensaje <nombre> tiene <edad> años, vive en <dirección> y su número de 
+teléfono es <teléfono>.'''
+
+# dat_nom = input("Coloca tu nombre: ")
+# dat_edad= input("Coloca tu edad: ")
+# dat_dir=input("Coloca tu dirección: ")
+# dat_tel=input("Coloca tu telefóno: ")
+
+# datos={"Nombre":dat_nom, "edad":dat_edad, "Dirección":dat_dir, "Telefono":dat_tel}
+
+# print(datos["Nombre"] + " tiene " + datos["edad"] + " años, vive en " + datos["Dirección"] + " y su telefono es " + datos["Telefono"])
+
+
+
+
