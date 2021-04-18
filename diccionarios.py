d6511829feef17ea17fb410a955362cb567fcdf0
Dante <dantevalles2016@yahoo.com>
2021-04-18 01:17:38 -0300

    sigue

--- diccionarios.py
+++ diccionarios.py
@@ -40,12 +40,63 @@ muestre por pantalla la misma fecha en formato dd de <mes> de aaaa donde
 #fecha = fecha.split('/')
 #print(fecha[0], 'de', meses[int(fecha[1])], 'de', fecha[2])
 
-'''Escribir un programa que almacene el diccionario con los créditos de las
- asignaturas de un curso {'Matemáticas': 6, 'Física': 4, 'Química': 5} y después
+'''5. Escribir un programa que almacene el diccionario con los créditos de las
+asignaturas de un curso {'Matemáticas': 6, 'Física': 4, 'Química': 5} y después
 muestre por pantalla los créditos de cada asignatura en el formato <asignatura>
 tiene <créditos> créditos, donde <asignatura> es cada una de las asignaturas del
- curso, y <créditos> son sus créditos. Al final debe mostrar también el número
-  total de créditos del curso.'''
+curso, y <créditos> son sus créditos. Al final debe mostrar también el número
+total de créditos del curso.'''
+
+#curso = {'Matemáticas': 6, 'Física': 4, 'Química': 5}
+#total_creditos = 0
+#for asignatura, creditos in curso.items():
+#    print(asignatura, 'tiene', creditos, 'créditos')
+#    total_creditos += creditos
+#print('Número total de créditos del curso: ', total_creditos)
+
+'''6. Escribir un programa que cree un diccionario vacío y lo vaya llenado con
+información sobre una persona (por ejemplo nombre, edad, sexo, teléfono, correo
+electrónico, etc.) que se le pida al usuario. Cada vez que se añada un nuevo 
+dato debe imprimirse el contenido del diccionario.'''
+
+#persona = {}
+#continuar = True
+#while continuar:
+#    clave = input('¿Qué dato quieres introducir? ')
+#    valor = input(clave + ': ')
+#    persona[clave] = valor
+#    print(persona)
+#    continuar = input('¿Quieres añadir más información (Si/No)? ').title() == "Si"
+
+'''7. Escribir un programa que cree un diccionario simulando una cesta de la
+ compra. El programa debe preguntar el artículo y su precio y añadir el par 
+ al diccionario, hasta que el usuario decida terminar. Después se debe mostrar
+  por pantalla la lista de la compra y el coste total, con el siguiente formato'''
+
+#compras={}
+#agregar=True
+#total=0
+#while agregar:
+#    clave=input("Articulo: ")
+#    valor=float(input(clave + ": precio: "))
+#    compras[clave]=valor
+#    agregar=input("agregas mas.?").title() == "Si"
+#for clave, valor in compras.items():
+#    print(clave, "\t", valor)
+#    total+=valor
+#print("total" "\t", total)
+
+'''8. Escribir un programa que cree un diccionario de traducción español-inglés.
+El usuario introducirá las palabras en español e inglés separadas por dos puntos,
+y cada par <palabra>:<traducción> separados por comas. El programa debe crear un
+diccionario con las palabras y sus traducciones. Después pedirá una frase en 
+español y utilizará el diccionario para traducirla palabra a palabra. Si una 
+palabra no está en el diccionario debe dejarla sin traducir.'''
+
+palabras={}
+
+for clave, valor in palabras:
+    
 
 
 
