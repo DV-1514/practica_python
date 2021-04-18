54108f507447eb958c6b6a933833f9d377692d28
Dante <dantevalles2016@yahoo.com>
2021-04-17 21:31:27 -0300

    Siguiendo

--- diccionarios.py
+++ diccionarios.py
@@ -35,8 +35,17 @@ en el diccionario debe mostrar un mensaje informando de ello.'''
 muestre por pantalla la misma fecha en formato dd de <mes> de aaaa donde 
 <mes> es el nombre del mes.'''
 
+#meses = {1:'enero', 2:'febrero', 3:'marzo', 4:'abril', 5:'mayo', 6:'junio', 7:'julio', 8:'agosto', 9:'septiembre', 10:'octubre', 11:'noviembre', 12:'diciembre'}
+#fecha = input('Introduce una fecha en formato dd/mm/aaaa: ')
+#fecha = fecha.split('/')
+#print(fecha[0], 'de', meses[int(fecha[1])], 'de', fecha[2])
 
-
+'''Escribir un programa que almacene el diccionario con los créditos de las
+ asignaturas de un curso {'Matemáticas': 6, 'Física': 4, 'Química': 5} y después
+muestre por pantalla los créditos de cada asignatura en el formato <asignatura>
+tiene <créditos> créditos, donde <asignatura> es cada una de las asignaturas del
+ curso, y <créditos> son sus créditos. Al final debe mostrar también el número
+  total de créditos del curso.'''
 
 
 
