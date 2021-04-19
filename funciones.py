93546e846373dcdb9817015f3b75c94a43a839f8
Dante <dantevalles2016@yahoo.com>
2021-04-19 15:41:27 -0300

    practica

---
+++ funciones.py
@@ -0,0 +1,39 @@
+'''1. Escribir una función que muestre por pantalla el saludo ¡Hola amiga!
+ cada vez que se la invoque.'''
+
+#def llamada():
+#    print("Hola Amigo")
+#    return
+#llamada()
+
+'''2. Escribir una función a la que se le pase una cadena <nombre> y 
+muestre por pantalla el saludo ¡hola <nombre>!.'''
+
+#def llamada():
+#    v=input("Coloca un nombre: ")
+#    print("Hola " + v)
+#    return
+#llamada()
+
+#def llamada(nombre):
+#    print("Hola "+ nombre)
+#    return
+#llamada("Dante")
+
+'''3. Escribir una función que reciba un número entero positivo y 
+devuelva su factorial.'''
+
+#def factorial(numero):
+#    num=1
+#    for i in range(numero):
+#        num *= i+1
+#    return num
+#print(factorial(5))
+#print(factorial(4))
+
+'''4.Escribir una función que calcule el total de una factura tras
+aplicarle el IVA. La función debe recibir la cantidad sin IVA y el 
+porcentaje de IVA a aplicar, y devolver el total de la factura. Si se 
+invoca la función sin pasarle el porcentaje de IVA, deberá aplicar un 21%.'''
+
+
